396dbedaee87dc9b2023f7280e9f2425bc9b0bbe
alldroll <alldroll@gmail.com>
2016-09-21 23:50:19 +1000

    init cluster

---
+++ clustering/cluster.go
@@ -0,0 +1,24 @@
+package cluster
+
+import (
+	"github.com/alldroll/quadtree/geometry"
+)
+
+/* declare */
+func DistanceBetweenPoints(a *shape.Point, b *shape.Point) float32 /*?*/
+
+type Cluster struct {
+	center *shape.Point
+	points []*shape.Point
+	bounds *shape.BoundaryBox
+}
+
+type PointClusterer struct {
+	clusters *[]Clusters
+}
+
+/*
+ */
+func (Cluster *self) addPoint(point *shape.Point) bool {
+
+}
